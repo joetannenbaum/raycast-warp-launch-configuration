711bc544df2528418f447deabb3221b3b4a1abbb
Joe Tannenbaum <joe@joe.codes>
2023-03-08 09:46:29 -0500

    publishing

--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,75 @@
-import { ActionPanel, Action, List } from "@raycast/api";
-import { useFetch, Response } from "@raycast/utils";
-import { useState } from "react";
-import { URLSearchParams } from "node:url";
+import { ActionPanel, Action, List, closeMainWindow } from "@raycast/api";
+import { useEffect, useState } from "react";
+import fs from "node:fs";
+import path from "node:path";
+import os from "node:os";
+import { runJxa } from "run-jxa";
 
 export default function Command() {
   const [searchText, setSearchText] = useState("");
-  const { data, isLoading } = useFetch(
-    "https://api.npms.io/v2/search?" +
-      // send the search query to the API
-      new URLSearchParams({ q: searchText.length === 0 ? "@raycast/api" : searchText }),
-    {
-      parseResponse: parseFetchResponse,
-    }
-  );
+  const [results, setResults] = useState<SearchResult[]>([]);
+
+  useEffect(() => {
+    const fullPath = path.join(os.homedir(), ".warp/launch_configurations");
+
+    fs.readdir(fullPath, (error, files) => {
+      if (error) {
+        console.log(error);
+      }
+
+      setResults(files.map((file) => ({ name: file.replace(".yaml", ""), path: path.join(fullPath, file) })));
+    });
+  }, []);
 
   return (
     <List
-      isLoading={isLoading}
+      isLoading={results.length === 0}
       onSearchTextChange={setSearchText}
-      searchBarPlaceholder="Search npm packages..."
+      searchBarPlaceholder="Searching for launch configurations..."
       throttle
     >
-      <List.Section title="Results" subtitle={data?.length + ""}>
-        {data?.map((searchResult) => (
-          <SearchListItem key={searchResult.name} searchResult={searchResult} />
-        ))}
+      <List.Section title="Results" subtitle={results?.length + ""}>
+        {results
+          ?.filter((f) => f.name.includes(searchText.toLowerCase()))
+          .map((searchResult) => (
+            <SearchListItem key={searchResult.name} searchResult={searchResult} />
+          ))}
       </List.Section>
     </List>
   );
 }
 
 function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
+  const appleScript = `const se = Application('System Events');
+    Application('Warp').activate();
+    se.keystroke('l', { using: ['command down', 'control down'] });
+    se.keystroke('${searchResult.name}');
+    se.keyCode(36);`;
+
   return (
     <List.Item
       title={searchResult.name}
-      subtitle={searchResult.description}
-      accessoryTitle={searchResult.username}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
-            <Action.OpenInBrowser title="Open in Browser" url={searchResult.url} />
+            <Action
+              title="Launch"
+              onAction={() => {
+                runJxa(appleScript);
+                closeMainWindow();
+              }}
+            />
           </ActionPanel.Section>
           <ActionPanel.Section>
-            <Action.CopyToClipboard
-              title="Copy Install Command"
-              content={`npm install ${searchResult.name}`}
+            <Action.ShowInFinder
+              title="Reveal in Finder"
+              path={searchResult.path}
               shortcut={{ modifiers: ["cmd"], key: "." }}
+            />
+            <Action.Open
+              title="Edit Launch Configuration"
+              target={searchResult.path}
+              shortcut={{ modifiers: ["cmd"], key: "o" }}
             />
           </ActionPanel.Section>
         </ActionPanel>
@@ -54,38 +78,7 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
   );
 }
 
-/** Parse the response from the fetch query into something we can display */
-async function parseFetchResponse(response: Response) {
-  const json = (await response.json()) as
-    | {
-        results: {
-          package: {
-            name: string;
-            description?: string;
-            publisher?: { username: string };
-            links: { npm: string };
-          };
-        }[];
-      }
-    | { code: string; message: string };
-
-  if (!response.ok || "message" in json) {
-    throw new Error("message" in json ? json.message : response.statusText);
-  }
-
-  return json.results.map((result) => {
-    return {
-      name: result.package.name,
-      description: result.package.description,
-      username: result.package.publisher?.username,
-      url: result.package.links.npm,
-    } as SearchResult;
-  });
-}
-
 interface SearchResult {
   name: string;
-  description?: string;
-  username?: string;
-  url: string;
+  path: string;
 }
